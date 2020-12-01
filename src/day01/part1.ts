ae6ca665ab246af079d9329f6858c8f3a9035add
izexi <43889168+izexi@users.noreply.github.com>
2020-12-01 20:50:57 +0000

    src: Day 1 - Report Repair

--- src/day01/part1.ts
+++ src/day01/part1.ts
@@ -2,4 +2,25 @@ import { parseInput } from '../util';
 
 const input = parseInput();
 
-// TODO: Complete Part 1
+export const multiplyEntries = (entries: number[]) =>
+  entries.reduce((p, c) => p * c);
+
+export default multiplyEntries(
+  input.filter((f) => input.find((s) => f + s === 2020))
+);
+
+/*  A more efficient solution:
+
+let solution;
+const nums = new Set(input);
+
+finding: for (const f of nums) {
+  nums.delete(f);
+  const s = 2020 - f;
+  if (nums.has(s)) {
+    solution = f * s;
+    break finding;
+  }
+}
+
+*/
--- src/day01/part2.ts
+++ src/day01/part2.ts
@@ -1,5 +1,26 @@
 import { parseInput } from '../util';
+import { multiplyEntries } from './part1';
 
 const input = parseInput();
 
-// TODO: Complete Part 2
+export default multiplyEntries(
+  input.filter((f) => input.find((s) => input.find((t) => f + s + t === 2020)))
+);
+
+/*  A more efficient solution:
+
+let solution;
+const nums = new Set(input);
+
+finding: for (const f of nums) {
+  nums.delete(f);
+  for (const s of nums) {
+    const t = 2020 - f - s;
+    if (nums.has(t)) {
+      solution = f * s * t;
+      break finding;
+    }
+  }
+}
+
+*/
