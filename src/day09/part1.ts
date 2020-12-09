cb69b830b2edf902cdf26ccfb30b29e89d82de5e
izexi <43889168+izexi@users.noreply.github.com>
2020-12-09 14:07:06 +0000

    src: Day 9 - Encoding Error

--- src/day09/part1.ts
+++ src/day09/part1.ts
@@ -2,4 +2,11 @@ import { parseInput } from '../util';
 
 const input = parseInput();
 
-// TODO: Complete Part 1
+export default input
+  .slice(25)
+  .find(
+    (num, i) =>
+      !input
+        .slice(i, i + 25)
+        .some((otherNum, _, arr) => arr.includes(num - otherNum))
+  );
--- src/day09/part2.ts
+++ src/day09/part2.ts
@@ -1,5 +1,18 @@
 import { parseInput } from '../util';
+import invalidNum from './part1';
 
 const input = parseInput();
+let encWeakness;
 
-// TODO: Complete Part 2
+for (let i = 0; i < input.length; i++) {
+  for (let j = i + 2; j < input.length + 1; j++) {
+    const contigiousArr = input.slice(i, j);
+
+    if (contigiousArr.reduce((prev, cur) => prev + cur, 0) === invalidNum) {
+      encWeakness = Math.min(...contigiousArr) + Math.max(...contigiousArr);
+      break;
+    }
+  }
+}
+
+export default encWeakness;
