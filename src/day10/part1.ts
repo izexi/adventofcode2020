ce0c6ffc99fe66bf9c86b6710d440133ffe91d04
izexi <43889168+izexi@users.noreply.github.com>
2020-12-10 21:46:41 +0000

    src: Day 10 - Adapter Array

--- src/day10/part1.ts
+++ src/day10/part1.ts
@@ -2,4 +2,14 @@ import { parseInput } from '../util';
 
 const input = parseInput();
 
-// TODO: Complete Part 1
+const [oneDiffs, , threeDiffs] = input
+  .sort((prev, cur) => prev - cur)
+  .reduce(
+    (diffs, num, i) => {
+      diffs[num - input[i - 1] - 1]++;
+      return diffs;
+    },
+    [1, 0, 1]
+  );
+
+export default oneDiffs * threeDiffs;
--- src/day10/part2.ts
+++ src/day10/part2.ts
@@ -1,5 +1,16 @@
 import { parseInput } from '../util';
 
 const input = parseInput();
+input.sort((prev, cur) => prev - cur).unshift(0);
 
-// TODO: Complete Part 2
+const numOfPaths = input.reduce(
+  (paths, n, i) => {
+    for (let j = i - 3; j < i; j++) {
+      if (n - input[j] <= 3) paths[i] += paths[j];
+    }
+    return paths;
+  },
+  input.map((_, i) => Number(i === 0))
+);
+
+export default numOfPaths.pop();
