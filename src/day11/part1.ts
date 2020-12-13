a61ab8f40cfcf6b7c734ac92d5484dcdaa45c806
izexi <43889168+izexi@users.noreply.github.com>
2020-12-13 14:07:51 +0000

    src: Day 11 - Seating System

--- src/day11/part1.ts
+++ src/day11/part1.ts
@@ -1,5 +1,49 @@
 import { parseInput } from '../util';
 
-const input = parseInput();
+const input = parseInput({ split: { mapper: (seats) => [...seats] } });
 
-// TODO: Complete Part 1
+export const DIRECTIONS = [
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, 1],
+  [1, 1],
+  [1, 0],
+  [1, -1],
+  [0, -1],
+];
+let seats = input;
+let occupiedSeats = 0;
+let stablized = false;
+
+while (!stablized) {
+  occupiedSeats = 0;
+  stablized = true;
+
+  seats = seats.map((rowOfSeats, column) =>
+    rowOfSeats.map((seat, row) => {
+      if (seat === '.') return seat;
+
+      const adjOccSeats = DIRECTIONS.reduce(
+        (total, [colOffset, rowOffset]) =>
+          total + Number(seats[column + colOffset]?.[row + rowOffset] === '#'),
+        0
+      );
+      const isEmpty = seat === 'L';
+
+      if (isEmpty ? !adjOccSeats : adjOccSeats >= 4) {
+        stablized = false;
+        if (isEmpty) {
+          occupiedSeats++;
+          return '#';
+        }
+        return 'L';
+      }
+
+      occupiedSeats += Number(!isEmpty);
+      return seat;
+    })
+  );
+}
+
+export default occupiedSeats;
--- src/day11/part2.ts
+++ src/day11/part2.ts
@@ -1,5 +1,47 @@
 import { parseInput } from '../util';
+import { DIRECTIONS } from './part1';
 
-const input = parseInput();
+const input = parseInput({ split: { mapper: (seats) => [...seats] } });
 
-// TODO: Complete Part 2
+let seats = input;
+let occupiedSeats = 0;
+let stablized = false;
+
+while (!stablized) {
+  occupiedSeats = 0;
+  stablized = true;
+
+  seats = seats.map((rowOfSeats, column) =>
+    rowOfSeats.map((seat, row) => {
+      if (seat === '.') return seat;
+
+      const adjOccSeats = DIRECTIONS.reduce((total, [colOffset, rowOffset]) => {
+        let seat;
+        let [_row, _column] = [row, column];
+
+        while ((seat = seats[(_column += colOffset)]?.[(_row += rowOffset)])) {
+          if (seat === 'L') return total;
+          if (seat === '#') return total + 1;
+        }
+
+        return total;
+      }, 0);
+
+      const isEmpty = seat === 'L';
+
+      if (isEmpty ? !adjOccSeats : adjOccSeats >= 5) {
+        stablized = false;
+        if (isEmpty) {
+          occupiedSeats++;
+          return '#';
+        }
+        return 'L';
+      }
+
+      occupiedSeats += Number(!isEmpty);
+      return seat;
+    })
+  );
+}
+
+export default occupiedSeats;
