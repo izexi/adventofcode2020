c0efbfbf2fe0a2a7317cf21539cf924ec061b231
izexi <43889168+izexi@users.noreply.github.com>
2020-12-15 16:44:22 +0000

    src: Day 12 - Rain Risk

--- src/day12/part1.ts
+++ src/day12/part1.ts
@@ -1,5 +1,49 @@
 import { parseInput } from '../util';
 
-const input = parseInput();
+export const input = parseInput({
+  split: {
+    mapper: ([direction, ...value]) => [direction, Number(value.join(''))],
+  },
+}) as [keyof typeof DIRECTIONS, number][];
 
-// TODO: Complete Part 1
+export const DIRECTIONS = {
+  N: [0, 1],
+  E: [1, 0],
+  S: [0, -1],
+  W: [-1, 0],
+  R: [1],
+  L: [-1],
+};
+
+const {
+  position: [east, north],
+} = input.reduce(
+  ({ position: [_east, _north], angle }, [direction, value]) => {
+    const [x, y] = Object.values(DIRECTIONS)[angle / 90];
+    value *= DIRECTIONS[direction]?.find((d) => d) ?? 1;
+
+    switch (direction) {
+      case 'N':
+      case 'S':
+        _north += value;
+        break;
+      case 'E':
+      case 'W':
+        _east += value;
+        break;
+      case 'R':
+      case 'L':
+        angle = (angle + value + 360) % 360;
+        break;
+      default:
+        _east += x * value;
+        _north += y * value;
+        break;
+    }
+
+    return { position: [_east, _north], angle };
+  },
+  { position: [0, 0], angle: 90 }
+);
+
+export default Math.abs(east) + Math.abs(north);
--- src/day12/part2.ts
+++ src/day12/part2.ts
@@ -1,5 +1,47 @@
-import { parseInput } from '../util';
+import { DIRECTIONS, input } from './part1';
 
-const input = parseInput();
+const ROTATIONS: {
+  [key: number]: (east: number, north: number) => [number, number];
+} = {
+  0: (east, north) => [east, north],
+  90: (east, north) => [north, -east],
+  180: (east, north) => [-east, -north],
+  270: (east, north) => [-north, east],
+  360: (east, north) => [east, north],
+};
 
-// TODO: Complete Part 2
+const {
+  position: [east, north],
+} = input.reduce(
+  (
+    { position: [pEast, pNorth], waypoint: [wEast, wNorth], angle },
+    [direction, value]
+  ) => {
+    value *= DIRECTIONS[direction]?.find((d) => d) ?? 1;
+
+    switch (direction) {
+      case 'N':
+      case 'S':
+        wNorth += value;
+        break;
+      case 'E':
+      case 'W':
+        wEast += value;
+        break;
+      case 'R':
+      case 'L':
+        angle = (value + 360) % 360;
+        [wEast, wNorth] = ROTATIONS[angle](wEast, wNorth);
+        break;
+      default:
+        pEast += value * wEast;
+        pNorth += value * wNorth;
+        break;
+    }
+
+    return { position: [pEast, pNorth], waypoint: [wEast, wNorth], angle };
+  },
+  { position: [0, 0], waypoint: [10, 1], angle: 0 }
+);
+
+export default Math.abs(east) + Math.abs(north);
