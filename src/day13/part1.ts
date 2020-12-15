24bd417c1bc96b3008ed4549bc6e529aa3d7818f
izexi <43889168+izexi@users.noreply.github.com>
2020-12-15 20:43:37 +0000

    src: Day 13 - Shuttle Search

--- src/day13/part1.ts
+++ src/day13/part1.ts
@@ -1,5 +1,18 @@
 import { parseInput } from '../util';
 
-const input = parseInput();
+const input = parseInput({
+  split: {
+    mapper: (line, i) =>
+      i ? line.split(',').map(Number).filter(Boolean) : Number(line),
+  },
+}) as [number, number[]];
 
-// TODO: Complete Part 1
+const [earliest, ids] = input;
+const maxId = Math.max(...ids);
+
+const earliestBusTime = Array.from(
+  { length: earliest + maxId + 1 },
+  (_, i) => earliest + i
+).find((time) => ids.some((id) => !(time % id)))!;
+
+export default maxId * (earliestBusTime - 1013728);
--- src/day13/part2.ts
+++ src/day13/part2.ts
@@ -1,5 +1,22 @@
 import { parseInput } from '../util';
 
-const input = parseInput();
+const input = parseInput({
+  split: {
+    mapper: (line, i) => (i ? line.split(',').map(Number) : Number(line)),
+  },
+}) as [number, number[]];
 
-// TODO: Complete Part 2
+const mod = (n: number, m: number) => ((n % m) + m) % m;
+
+const [, ids] = input;
+const buses = ids.filter(Boolean).map((id) => ({
+  id: BigInt(id),
+  offset: BigInt(mod(id - ids.indexOf(id), id)),
+}));
+
+const { offset } = buses.reduce(({ offset, id }, bus) => {
+  while (offset % bus.id !== bus.offset) offset += id;
+  return { offset, id: id * bus.id };
+});
+
+export default offset;
