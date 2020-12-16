da06336258cbd3dcf3bce37033315f773b1e58c9
izexi <43889168+izexi@users.noreply.github.com>
2020-12-16 03:26:50 +0000

    src: Day 14 - Docking Data

--- src/day14/part1.ts
+++ src/day14/part1.ts
@@ -1,5 +1,33 @@
 import { parseInput } from '../util';
 
-const input = parseInput();
+export const decToBin = (n: string) => Number(n).toString(2).padStart(36, '0');
+export const sumMemory = (memoryMap: Map<string, number>[]) =>
+  Object.values(
+    Object.fromEntries(memoryMap.flatMap((memory) => [...memory]))
+  ).reduce((prev, cur) => prev + cur);
 
-// TODO: Complete Part 1
+const input = parseInput({
+  split: {
+    delimiter: 'mask = ',
+    mapper: (program: string) => {
+      const [mask, ...lines] = program.split('\n').filter(Boolean);
+      return lines.reduce((memory: Map<string, number>, line) => {
+        const { address, value } = line.match(
+          /mem\[(?<address>\d+)\] = (?<value>\d+)/
+        )!.groups!;
+
+        return memory.set(
+          address,
+          parseInt(
+            [...decToBin(value)]
+              .map((b, i) => (mask[i] === 'X' ? b : mask[i]))
+              .join(''),
+            2
+          )
+        );
+      }, new Map());
+    },
+  },
+});
+
+export default sumMemory(input);
--- src/day14/part2.ts
+++ src/day14/part2.ts
@@ -1,5 +1,42 @@
 import { parseInput } from '../util';
+import { decToBin, sumMemory } from './part1';
 
-const input = parseInput();
+const input = parseInput({
+  split: {
+    delimiter: 'mask = ',
+    mapper: (program: string) => {
+      const [mask, ...lines] = program.split('\n').filter(Boolean);
+      return lines.reduce((memory: Map<string, number>, line) => {
+        const { address, value } = line.match(
+          /mem\[(?<address>\d+)\] = (?<value>\d+)/
+        )!.groups!;
+        const binExp = mask.split('X').length - 1;
 
-// TODO: Complete Part 2
+        for (let i = 0; i < 2 ** binExp; i++) {
+          const floatingBit = [...i.toString(2).padStart(binExp, '0')];
+
+          memory.set(
+            [...decToBin(address)]
+              .map((b, j) => {
+                const currBit = mask[j];
+                if (currBit === 'X') return floatingBit.shift()!;
+                if (currBit === '1') return '1';
+                return b;
+              })
+              .join(''),
+            parseInt(
+              [...decToBin(value)]
+                .map((b, i) => (mask[i] === 'X' ? b : mask[i]))
+                .join(''),
+              2
+            )
+          );
+        }
+
+        return memory;
+      }, new Map());
+    },
+  },
+});
+
+export default sumMemory(input);
