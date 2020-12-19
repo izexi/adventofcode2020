e1c355287dae186006dba81be291c6d93c85bb5c
izexi <43889168+izexi@users.noreply.github.com>
2020-12-19 17:32:07 +0000

    src: Day 19 - Monster Messages

--- src/day19/part1.ts
+++ src/day19/part1.ts
@@ -1,5 +1,22 @@
 import { parseInput } from '../util';
 
-const input = parseInput();
+const input = parseInput({ split: { delimiter: '\n\n', mapper: false } });
 
-// TODO: Complete Part 1
+const [rules, messages] = input;
+export const rulesMap = new Map(
+  rules.split('\n').map((rule) => rule.split(': ')) as [string, string][]
+);
+
+const createPattern = (ruleNum: string): string => {
+  const rule = rulesMap.get(ruleNum)!;
+  return (
+    rule.match(/"(a|b)"/)?.[1] ??
+    `(${rule
+      .split(' | ')
+      .map((subRule) => subRule.split(' ').map(createPattern).join(''))
+      .join('|')})`
+  );
+};
+
+export default messages.match(new RegExp(`^${createPattern('0')}$`, 'gm'))!
+  .length;
--- src/day19/part2.ts
+++ src/day19/part2.ts
@@ -1,5 +1,42 @@
 import { parseInput } from '../util';
 
-const input = parseInput();
+const input = parseInput({ split: { delimiter: '\n\n', mapper: false } });
+const [rules, messages] = input;
 
-// TODO: Complete Part 2
+const findMatches = (prevMatches = 0, length = 1): number => {
+  const rulesMap = new Map(
+    rules.split('\n').map((rule) => rule.split(': ')) as [string, string][]
+  );
+
+  const createPattern = (ruleNum: string): string => {
+    const rule = rulesMap.get(ruleNum)!;
+    const aOrB = rule.match(/"(a|b)"/)?.[1];
+
+    if (aOrB) return aOrB;
+    if (rule.startsWith('(')) return rule;
+
+    const pattern = `(${rule
+      .split(' | ')
+      .map((subRule) => subRule.split(' ').map(createPattern).join(''))
+      .join('|')})`;
+
+    rulesMap.set(ruleNum, pattern);
+    return pattern;
+  };
+
+  rulesMap.set('8', `(${createPattern('42')}+)`);
+  rulesMap.set(
+    '11',
+    `(${Array.from(
+      { length },
+      (_, i) =>
+        `(${createPattern('42')}{${i + 1}}${createPattern('31')}{${i + 1}})`
+    ).join('|')})`
+  );
+
+  const matches = messages.match(new RegExp(`^${createPattern('0')}$`, 'gm'))!
+    .length;
+  return matches === prevMatches ? matches : findMatches(matches, ++length);
+};
+
+export default findMatches();
