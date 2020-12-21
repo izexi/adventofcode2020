1d60712a107ce2b82317756c910853b120b77771
izexi <43889168+izexi@users.noreply.github.com>
2020-12-21 19:15:47 +0000

    src: Day 20 - Jurassic Jigsaw (Part 1)

--- src/day20/part1.ts
+++ src/day20/part1.ts
@@ -1,5 +1,51 @@
 import { parseInput } from '../util';
 
-const input = parseInput();
+const input = parseInput({
+  split: {
+    delimiter: '\n\n',
+    mapper: (tile: string) => {
+      const { id, image } = tile.match(
+        /Tile (?<id>\d+):\n(?<image>[\s\S]*)/
+      )!.groups!;
+      return [Number(id), image.split('\n')];
+    },
+  },
+});
 
-// TODO: Complete Part 1
+const tilesMap = new Map(input as [number, string[]][]);
+const edgeAndFlipped = (edge: string | string[]) => [
+  Array.isArray(edge) ? edge.join('') : edge,
+  [...edge].reverse().join(''),
+];
+
+const edgesAtId = [...tilesMap.entries()].reduce(
+  (map: Map<number, string[]>, [id, tiles]) =>
+    map.set(
+      id,
+      [
+        edgeAndFlipped(tiles[0]),
+        edgeAndFlipped(tiles.slice().pop()!),
+        edgeAndFlipped(tiles.map(([left]) => left)),
+        edgeAndFlipped(tiles.map((row) => row.slice(-1))),
+      ].flat()
+    ),
+  new Map()
+);
+
+export default [...edgesAtId.entries()].reduce(
+  (mulId, [id, edges], _, entries) => {
+    return (mulId *=
+      new Set(
+        edges.flatMap((edge) =>
+          entries.reduce(
+            (ids: number[], [_id, _edges]) =>
+              _edges.includes(edge) ? ids.concat(_id) : ids,
+            []
+          )
+        )
+      ).size === 3
+        ? id
+        : 1);
+  },
+  1
+);
