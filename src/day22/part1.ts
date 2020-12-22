4c8c684d0ee68d433729415c0c41049f15b309a3
izexi <43889168+izexi@users.noreply.github.com>
2020-12-22 16:10:48 +0000

    src: Day 22 - Crab Combat

--- src/day22/part1.ts
+++ src/day22/part1.ts
@@ -1,5 +1,20 @@
 import { parseInput } from '../util';
 
-const input = parseInput();
+const input = parseInput({
+  split: {
+    delimiter: '\n\n',
+    mapper: (cards: string) => cards.match(/^(\d+)/gm)!.map(Number),
+  },
+});
 
-// TODO: Complete Part 1
+export const calcualteScore = (cards: number[]) =>
+  cards.reduce((score, card, i) => score + card * (cards.length - i), 0);
+
+const play = ([[p1Top], [p2Top]] = input): number => {
+  const winner = input[0].includes(Math.max(p1Top, p2Top)) ? 0 : 1;
+  input[winner].push(input[winner].shift()!, input[winner ^ 1].shift()!);
+
+  return input[winner ^ 1].length ? play() : calcualteScore(input[winner]);
+};
+
+export default play();
--- src/day22/part2.ts
+++ src/day22/part2.ts
@@ -1,5 +1,33 @@
 import { parseInput } from '../util';
+import { calcualteScore } from './part1';
 
-const input = parseInput();
+const input = parseInput({
+  split: {
+    delimiter: '\n\n',
+    mapper: (cards: string) => cards.match(/^(\d+)/gm)!.map(Number),
+  },
+}) as [number[], number[]];
 
-// TODO: Complete Part 2
+const play = (p1: number[], p2: number[]) => {
+  const playedCards = new Set();
+
+  while (p1.length && p2.length) {
+    const currentCards = `${p1.join(',')} ${p2.join(',')}`;
+    if (+playedCards.has(currentCards) | +playedCards.add(currentCards))
+      return 0;
+
+    const topCards = [p1.shift()!, p2.shift()!];
+    const [p1Top, p2Top] = topCards;
+    const winner = Number(
+      p1.length >= p1Top && p2.length >= p2Top
+        ? play(p1.slice(0, p1Top), p2.slice(0, p2Top))
+        : p1Top < p2Top
+    );
+
+    [p1, p2][winner].push(topCards[winner], topCards[winner ^ 1]);
+  }
+
+  return p1.length ? 0 : 1;
+};
+
+export default calcualteScore(input[play(...input)]);
