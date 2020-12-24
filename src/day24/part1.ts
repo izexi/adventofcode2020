588807a1ce892b0a90beeda855b744bd9b6b5b0a
izexi <43889168+izexi@users.noreply.github.com>
2020-12-24 18:36:06 +0000

    src: Day 24 - Lobby Layout

--- src/day24/part1.ts
+++ src/day24/part1.ts
@@ -1,5 +1,30 @@
 import { parseInput } from '../util';
 
-const input = parseInput();
+const input = parseInput({
+  split: { mapper: (line) => line.match(/e|se|sw|w|nw|ne/g)! },
+});
 
-// TODO: Complete Part 1
+export const DIRECTIONS = {
+  e: [1, 0],
+  w: [-1, 0],
+  ne: [0.5, -1],
+  nw: [-0.5, -1],
+  se: [0.5, 1],
+  sw: [-0.5, 1],
+};
+
+export const blackTiles = input.reduce((blackTiles, tiles) => {
+  const tile = tiles
+    .reduce(
+      ([x, y], direction) => {
+        const [xInc, yInc] = DIRECTIONS[direction as keyof typeof DIRECTIONS];
+        return [x + xInc, y + yInc];
+      },
+      [0, 0]
+    )
+    .toString();
+
+  return blackTiles.delete(tile) ? blackTiles : blackTiles.add(tile);
+}, new Set() as Set<string>);
+
+export default blackTiles.size;
--- src/day24/part2.ts
+++ src/day24/part2.ts
@@ -1,5 +1,27 @@
-import { parseInput } from '../util';
+import { blackTiles, DIRECTIONS } from './part1';
 
-const input = parseInput();
+let newBlackTiles = blackTiles;
 
-// TODO: Complete Part 2
+for (let day = 1; day <= 100; day++) {
+  const tileFreqs = [...newBlackTiles.values()].reduce(
+    (tileFreqs, blackTile) => {
+      const [x, y] = blackTile.split(',').map(Number);
+      Object.values(DIRECTIONS).forEach(([xInc, yInc]) => {
+        const tilePos = [x + xInc, y + yInc].toString();
+        tileFreqs.set(tilePos, (tileFreqs.get(tilePos) ?? 0) + 1);
+      });
+      return tileFreqs;
+    },
+    new Map()
+  );
+
+  newBlackTiles = [...tileFreqs.entries()].reduce(
+    (newBlacks, [tile, freq]) =>
+      freq === 2 || (newBlackTiles.has(tile) && freq === 1)
+        ? newBlacks.add(tile)
+        : newBlacks,
+    new Set() as Set<string>
+  );
+}
+
+export default newBlackTiles.size;
