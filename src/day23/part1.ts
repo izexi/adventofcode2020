9891e5b4e9a24cb15243f7e17c199e8c9dcafa4b
izexi <43889168+izexi@users.noreply.github.com>
2020-12-24 15:15:53 +0000

    src: Day 23 - Crab Cups

--- src/day23/part1.ts
+++ src/day23/part1.ts
@@ -1,5 +1,26 @@
 import { parseInput } from '../util';
 
-const input = parseInput();
+const input = parseInput({ split: { delimiter: '' } });
+const totalCups = input.length;
+let lastPicked = input[input.length - 1];
 
-// TODO: Complete Part 1
+for (let move = 1; move <= 100; move++) {
+  const currI = (input.indexOf(lastPicked) + 1) % input.length;
+  lastPicked = input[currI];
+
+  const nextThree = Array.from({ length: 3 }, () => {
+    const index = (currI + 1) % totalCups;
+    return input.splice(index >= input.length ? 0 : index, 1);
+  }).flat();
+
+  const getDest = (destination = lastPicked - 1): number => {
+    if (destination < Math.min(...input)) return Math.max(...input);
+    if (nextThree.includes(destination)) return getDest(--destination);
+    return destination;
+  };
+
+  input.splice(input.indexOf(getDest()) + 1, 0, ...nextThree);
+}
+
+const [before1, after1] = input.join('').split('1');
+export default after1 + before1;
--- src/day23/part2.ts
+++ src/day23/part2.ts
@@ -1,5 +1,45 @@
 import { parseInput } from '../util';
 
-const input = parseInput();
+interface Cup {
+  label: number;
+  next: null | Cup;
+}
 
-// TODO: Complete Part 2
+const input = parseInput({ split: { delimiter: '' } });
+
+const cups: Cup[] = Array.from(
+  { length: 1000000 },
+  (_, i) => input[i] || 9 + (i - 8)
+)
+  .map((label) => ({ label, next: null }))
+  .map((cup, i, cups) =>
+    Object.assign(cup, { next: cups[(cups.length + i + 1) % cups.length] })
+  );
+const cupsMap = new Map(cups.map((cup) => [cup.label, cup]));
+let [lastPicked] = cups;
+
+for (let move = 1; move <= 10000000; move++) {
+  const nextCup = lastPicked.next!;
+  const nextThree = [
+    nextCup.label,
+    nextCup.next!.label,
+    nextCup.next!.next!.label,
+  ];
+  lastPicked.next = nextCup.next!.next!.next;
+
+  const getDest = (destination = lastPicked.label): number => {
+    if (destination === 1) return cupsMap.size;
+    return nextThree.includes(--destination)
+      ? getDest(destination)
+      : destination;
+  };
+  const destination = cupsMap.get(getDest())!;
+  const temp = destination.next;
+
+  destination.next = nextCup;
+  destination.next!.next!.next!.next = temp;
+  lastPicked = lastPicked.next!;
+}
+
+const cupAfter1 = cupsMap.get(1)!.next!;
+export default cupAfter1.label * cupAfter1.next!.label;
