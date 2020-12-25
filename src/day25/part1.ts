8611f0efa0bdf158bcf3fe4135ee404936840ad8
izexi <43889168+izexi@users.noreply.github.com>
2020-12-25 16:38:44 +0000

    src: Day 25 - Combo Breaker

--- src/day25/part1.ts
+++ src/day25/part1.ts
@@ -1,5 +1,39 @@
+/* eslint-disable */ 
 import { parseInput } from '../util';
 
-const input = parseInput();
+const input = parseInput(/*🎅*/);
+const [card, door] = input;
 
-// TODO: Complete Part 1
+const encryptionKey =//   ⭐ 
+                           (
+                         loop
+                       =0,value
+                     =1,loopSize =
+                   ((value=1,loopSize
+                =1)=>{ while((value =(
+               value*7)%20201227)!==card
+            &&++loopSize);return loopSize;}
+          )()) => {while (loop++ !== loopSize)
+        value=(value*door)%20201227;return value;
+      /*  🎁 🎁    🎁*/   }; // 🎁  🎁🎁🎁  🎁 *\
+    
+
+
+export default encryptionKey(/*⛄*/);
+
+/* 
+
+const encryptionKey = (
+  loop = 0,
+  value = 1,
+  loopSize = ((value = 1, loopSize = 1) => {
+    while ((value = (value * 7) % 20201227) !== card && ++loopSize);
+    return loopSize;
+  })()
+) => {
+  while (loop++ !== loopSize) value = (value * door) % 20201227;
+  return value;
+};
+
+*/
+
